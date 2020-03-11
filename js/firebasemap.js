8d4719379fec0165cbc082dc2507ff5eec204890
Jonathan Carroll <jonathaniscarroll@gmail.com>
2020-03-11 09:16:38 -0600

    heatmaps on map is working

--- js/firebasemap.js
+++ js/firebasemap.js
@@ -16,7 +16,7 @@ function initMap() {
 
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 0, lng: 0},
-    zoom: 3,
+    zoom: 18,
     styles: [{
       featureType: 'poi',
       stylers: [{ visibility: 'off' }]  // Turn off points of interest.
@@ -27,6 +27,36 @@ function initMap() {
     disableDoubleClickZoom: true,
     streetViewControl: false
   });
+
+  infoWindow = new google.maps.InfoWindow;
+
+  if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+
   // Create the DIV to hold the control and call the makeInfoBox() constructor
   // passing in this DIV.
   var infoBoxDiv = document.createElement('div');
@@ -130,7 +160,7 @@ function initFirebase(heatmap) {
       heatmap.getData().push(point);
 
       // Request entries older than expiry time (10 minutes).
-      var expiryMs = Math.max(60 * 10 * 1000 - elapsed, 0);
+      var expiryMs = Math.max(60 * 10 * 1000 - elapsedMs, 0);
       // Set client timeout to remove the point after a certain time.
       window.setTimeout(function() {
         // Delete the old point from the database.
@@ -159,3 +189,40 @@ clicks.orderByChild('timestamp').startAt(startTime).on('child_added',
   }
 );
 }
+
+function addToFirebase(data) {
+        getTimestamp(function(timestamp) {
+          // Add the new timestamp to the record data.
+          data.timestamp = timestamp;
+          var ref = firebase.database().ref('clicks').push(data, function(err) {
+            if (err) {  // Data was not written to firebase.
+              console.warn(err);
+            }
+          });
+        });
+      }
+            /**
+       * Updates the last_message/ path with the current timestamp.
+       * @param {function(Date)} addClick After the last message timestamp has been updated,
+       *     this function is called with the current timestamp to add the
+       *     click to the firebase.
+       */
+      function getTimestamp(addClick) {
+        // Reference to location for saving the last click time.
+        var ref = firebase.database().ref('last_message/' + data.sender);
+
+        ref.onDisconnect().remove();  // Delete reference from firebase on disconnect.
+
+        // Set value to timestamp.
+        ref.set(firebase.database.ServerValue.TIMESTAMP, function(err) {
+          if (err) {  // Write to last message was unsuccessful.
+            console.log(err);
+          } else {  // Write to last message was successful.
+            ref.once('value', function(snap) {
+              addClick(snap.val());  // Add click with same timestamp.
+            }, function(err) {
+              console.warn(err);
+            });
+          }
+        });
+      }
